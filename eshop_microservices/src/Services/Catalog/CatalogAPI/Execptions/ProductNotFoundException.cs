305ea21bcd7318587c922afa7d5552c1a5c70820
Ajay Bohra <Ajaybohra000@gmail.com>
2025-08-11 19:27:41 +0530

    initial

---
+++ eshop_microservices/src/Services/Catalog/CatalogAPI/Execptions/ProductNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace CatalogAPI.Execptions
+{
+    public class ProductNotFoundException:Exception
+    {
+        public ProductNotFoundException():base("Product not found")
+        {
+        }
+    }
+}
---
+++ eshop_microservices/src/Services/Catalog/CatalogAPI/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,29 @@
+﻿
+namespace CatalogAPI.Products.UpdateProduct
+{
+    public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>;
+    public record UpdateProductResult(bool IsSuccess);
+
+    public class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+            : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("UpdateProductHandler. Handle called with {@Command}", command);
+            var Product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+            if (Product == null)
+            {
+                logger.LogWarning("Product with Id {ProductId} not found", command.Id);
+                throw new ProductNotFoundException();
+            }
+            Product.Name = command.Name;
+            Product.Category = command.Category;
+            Product.Description = command.Description;
+            Product.ImageFile = command.ImageFile;
+            Product.Price = command.Price;
+            session.Update(Product);
+            await session.SaveChangesAsync(cancellationToken);
+            return new UpdateProductResult(true);
+        }
+    }
+}
