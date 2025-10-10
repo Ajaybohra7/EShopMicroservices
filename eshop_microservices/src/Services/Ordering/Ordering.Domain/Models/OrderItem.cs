1318bb20efa43a3c750eaf6d9712c90a6e82e8cd
Ajay Bohra <Ajaybohra000@gmail.com>
2025-10-11 01:16:09 +0530

    Added sql as a bscking service for ordering

--- eshop_microservices/src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ eshop_microservices/src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -8,7 +8,7 @@ namespace Ordering.Domain.Models
         public int Quantity { get; private set; }
         public ProductId ProductId { get; private set; }=default!;
         public OrderId OrderId { get; private set; }=default!;
-        internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price )
+        public OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price )
         {
             Id=OrderItemId.Of(Guid.NewGuid()); 
             Price = price;
--- eshop_microservices/src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ eshop_microservices/src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Ordering.Domain.Models
 {
-    internal class Product:Entity<ProductId>
+    public class Product:Entity<ProductId>
     {
         public string Name { get; private set; }=default!;
         public decimal Price { get; private set; }= default!;
